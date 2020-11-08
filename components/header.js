eb51faaec8a5a2f0f3db68087909ad213c0268c9
shreyas gupta <shreyas.charming@gmail.com>
2020-11-08 09:18:57 +0530

    customised header

--- components/header.js
+++ components/header.js
@@ -1,12 +1,12 @@
 import Link from "next/link";
+import Logo from "./../public/logo.svg";
 
 export default function Header() {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <Link href="/">
-        <a className="hover:underline">Shreyas Gupta</a>
+        <img src={Logo} />
       </Link>
-      .
     </h2>
   );
 }
