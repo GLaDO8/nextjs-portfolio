9adce4d882843e51beb4444a6988d62ea5b55ec4
shreyas gupta <shreyas.charming@gmail.com>
2020-11-11 18:31:03 +0530

    added link affordance

--- components/navbutton.js
+++ components/navbutton.js
@@ -4,7 +4,7 @@ import { withRouter } from "next/router";
 const NavButton = (props) => (
   <Link href={props.path}>
     <div
-      className={`menu-btn-lg ${
+      className={`menu-btn-lg cursor-pointer ${
         props.router.pathname === props.path ? "menu-btn-lg-active" : ""
       }`}
     >
