b6e35ac2b02cd03366f805279671d70f72c25b77
shreyas gupta <shreyas.charming@gmail.com>
2020-11-18 09:45:46 +0530

    added footer content

--- components/footer.js
+++ components/footer.js
@@ -1,3 +1,18 @@
+import React from "react";
+
 export default function Footer() {
-  return <footer className="bg-accent-1 border-t border-accent-2"></footer>;
+  return <footer className="">
+    <>
+      <div className="mx-auto mx-w-lg text-center text-gray-600">
+      © Copyright 2020
+      </div>
+      <div className="w-full lg:flex lg:items-center lg:w-auto hidden">
+        <div className="lg:flex justify-center flex-1 items-center">
+          <div className="text-gray-600 font-semibold p-2">META</div>
+          <div className="text-gray-600 font-semibold p-2">SOURCE</div>
+        </div>
+      </div>
+    </>
+  </footer>;
 }
+
