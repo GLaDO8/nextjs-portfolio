558e60ca85db8b2b529673be1bcc2f4a7d6bfc9a
shreyas gupta <shreyas.charming@gmail.com>
2020-11-18 09:44:14 +0530

    changes to tailwind design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,42 @@
 module.exports = {
-  purge: ["./components/**/*.js", "./pages/**/*.js"],
+  purge: ['./components/**/*.js', './pages/**/*.js'],
   theme: {
     fontFamily: {
-      title: ["Chromate", "sans-serif"],
+      title: ['Rubik', 'sans-serif'],
+      sans: ['Rubik', 'sans-serif'],
     },
     extend: {
       colors: {
-        "accent-1": "#FAFAFA",
-        "accent-2": "#EAEAEA",
-        "accent-7": "#333",
-        success: "#0070f3",
-        cyan: "#79FFE1",
+        'accent-1': '#FAFAFA',
+        'accent-2': '#EAEAEA',
+        'accent-7': '#333',
+        success: '#0070f3',
+        cyan: '#79FFE1',
       },
       spacing: {
-        28: "7rem",
+        28: '7rem',
       },
       letterSpacing: {
-        tighter: "-.04em",
+        tighter: '-.04em',
       },
       lineHeight: {
         tight: 1.2,
       },
       fontSize: {
-        "5xl": "2.5rem",
-        "6xl": "2.75rem",
-        "7xl": "4.5rem",
-        "8xl": "6.25rem",
+        'lg': '1.075rem',
+        '2xl': '1.6rem',
+        '5xl': '2.5rem',
+        '6xl': '2.75rem',
+        '7xl': '4.5rem',
+        '8xl': '6.25rem',
+      },
+      maxWidth: {
+        '2xl': '44rem',
       },
       boxShadow: {
-        small: "0 5px 10px rgba(0, 0, 0, 0.12)",
-        medium: "0 8px 30px rgba(0, 0, 0, 0.12)",
+        small: '0 5px 10px rgba(0, 0, 0, 0.12)',
+        medium: '0 8px 30px rgba(0, 0, 0, 0.12)',
       },
     },
   },
-};
+}
