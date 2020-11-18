fe85d6ac8c565ebc1a5aa50d9418e1368e8f5c57
shreyas gupta <shreyas.charming@gmail.com>
2020-11-18 09:47:06 +0530

    updated homepage

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,65 @@
-import MoreStories from "@/components/more-stories";
-import HeroPost from "@/components/hero-post";
-import Intro from "@/components/navbar";
-import ButtonList from "@/components/buttonlist";
-import Layout from "@/components/layout";
-import { getAllPostsForHome } from "@/lib/api";
-import Head from "next/head";
-
-export default function Index({ allPosts }) {
-  const heroPost = allPosts[0];
-  const morePosts = allPosts.slice(1);
-  console.log(allPosts);
+import NavButton from '@/components/navbutton'
+import Layout from '@/components/layout'
+import Logo from './../public/newlogo.svg'
+import Head from 'next/head'
+import Buttonlist from '@/components/buttonlist'
+import MoreStories from '@/components/more-stories'
+import { getAllPostsForHome, getAllProjectsWithSlug } from '@/lib/api'
+export default function Index({ allPosts, allWork }) {
   return (
     <>
       <Layout>
         <Head>
           <title>Home</title>
         </Head>
-        <Intro navButtons={ButtonList} />
-        {heroPost && (
-          <HeroPost
-            title={heroPost.title}
-            coverImage={heroPost.metadata.cover_image}
-            date={heroPost.created_at}
-            author={heroPost.metadata.author}
-            slug={heroPost.slug}
-            excerpt={heroPost.metadata.excerpt}
-          />
-        )}
-        {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+        <Logo className="h-24 md:h-32 lg:h-40 mx-auto mt-24" />
+        <div
+          className="w-full lg:flex lg:items-center lg:w-auto hidden pt-6 lg:pt-6"
+          id="nav-content"
+        >
+          <div className="lg:flex justify-center flex-1 items-center">
+            {Buttonlist.map((button) => (
+              <NavButton
+                key={button.path}
+                path={button.path}
+                label={button.label}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="">
+          <div className="text-2xl md:text-3xl lg:text-4xl font-bold mb-8 mt-24 mx-auto max-w-lg text-center">
+            Hello, netizen.
+          </div>
+          <div className="text-lg md:text-xl lg:text-2xl font-regular mx-auto max-w-xl text-center">
+            I’m Shreyas Gupta, a designer with a knack for building things.
+            Scroll down to view my work.
+          </div>
+        </div>
+        <div>Work</div>
+        {/* <MoreStories posts={allWork} /> */}
+        <div>Writings</div>
+        <MoreStories posts={allPosts} />
+        <div>
+          <div className="text-6xl font-bold mx-auto max-w-2xl text-center">
+            For work enquiries, freelancing or just a friendly chat,
+          </div>
+        </div>
       </Layout>
     </>
-  );
+  )
 }
 
 export async function getStaticProps({ preview }) {
-  const allPosts = (await getAllPostsForHome(preview)) || [];
+  const allPosts = (await getAllPostsForHome(preview)) || []
   return {
     props: { allPosts },
-  };
+  }
 }
+
+// export async function getStaticProps({ preview }) {
+//   const allWork = (await getAllProjectsWithSlug(preview)) || []
+//   return {
+//     props: { allWork },
+//   }
+// }
