9fdaef05a1f06088539942a4526eadd2b9d9ae33
shreyas gupta <shreyas.charming@gmail.com>
2020-11-26 12:01:24 +0530

    removed twitter embed

--- components/twitter-masonry.js
+++ components/twitter-masonry.js
@@ -1,75 +1,75 @@
-import Masonry from 'react-masonry-css'
-import { TwitterTweetEmbed } from 'react-twitter-embed'
+// import Masonry from 'react-masonry-css'
+// import { TwitterTweetEmbed } from 'react-twitter-embed'
 
-const breakpointColumnsObj = {
-  default: 4,
-  1400: 3,
-  1100: 2,
-  800: 1,
-}
+// const breakpointColumnsObj = {
+//   default: 4,
+//   1400: 3,
+//   1100: 2,
+//   800: 1,
+// }
 
-export default function MasonryGridTwitter({ allPosts, allWork }) {
-  return (
-    <>
-      <div className="text-6xl font-bold mx-auto max-w-2xl text-center">
-        Thought Board
-      </div>
-      <Masonry
-        breakpointCols={breakpointColumnsObj}
-        className="my-masonry-grid"
-        columnClassName="my-masonry-grid_column"
-      >
-        {[
-          <div className="mb-8">
-            <TwitterTweetEmbed
-              tweetId={'1328567717079740417'}
-              options={{ cards: 'hidden', conversation: 'none', dnt: 'true' }}
-            />
-          </div>,
-          <div className="mb-8">
-            <TwitterTweetEmbed
-              tweetId={'1325808622295642112'}
-              options={{ cards: 'hidden', conversation: 'none', dnt: 'true' }}
-            />
-          </div>,
-          <div className="mb-8">
-            <TwitterTweetEmbed
-              tweetId={'1311586325900877827'}
-              options={{ cards: 'hidden', conversation: 'none', dnt: 'true' }}
-            />
-          </div>,
-          <div className="mb-8">
-            <TwitterTweetEmbed
-              tweetId={'1297199166490206208'}
-              options={{ dnt: 'true' }}
-            />
-          </div>,
-          <div className="mb-8">
-            <TwitterTweetEmbed
-              tweetId={'1328392965484515328'}
-              options={{ cards: 'hidden', conversation: 'none', dnt: 'true' }}
-            />
-          </div>,
-          <div className="mb-8">
-            <TwitterTweetEmbed
-              tweetId={'1321438656809218048'}
-              options={{ cards: 'hidden', conversation: 'none', dnt: 'true' }}
-            />
-          </div>,
-          <div className="mb-8">
-            <TwitterTweetEmbed
-              tweetId={'1294237220623114240'}
-              options={{ cards: 'hidden', conversation: 'none', dnt: 'true' }}
-            />
-          </div>,
-          <div className="mb-8">
-            <TwitterTweetEmbed
-              tweetId={'1275670374668218369'}
-              options={{ cards: 'hidden', conversation: 'none', dnt: 'true' }}
-            />
-          </div>,
-        ]}
-      </Masonry>
-    </>
-  )
-}
+// export default function MasonryGridTwitter({ allPosts, allWork }) {
+//   return (
+//     <>
+//       <div className="text-6xl font-bold mx-auto max-w-2xl text-center">
+//         Thought Board
+//       </div>
+//       <Masonry
+//         breakpointCols={breakpointColumnsObj}
+//         className="my-masonry-grid"
+//         columnClassName="my-masonry-grid_column"
+//       >
+//         {[
+//           <div className="mb-8">
+//             <TwitterTweetEmbed
+//               tweetId={'1328567717079740417'}
+//               options={{ cards: 'hidden', conversation: 'none', dnt: 'true' }}
+//             />
+//           </div>,
+//           <div className="mb-8">
+//             <TwitterTweetEmbed
+//               tweetId={'1325808622295642112'}
+//               options={{ cards: 'hidden', conversation: 'none', dnt: 'true' }}
+//             />
+//           </div>,
+//           <div className="mb-8">
+//             <TwitterTweetEmbed
+//               tweetId={'1311586325900877827'}
+//               options={{ cards: 'hidden', conversation: 'none', dnt: 'true' }}
+//             />
+//           </div>,
+//           <div className="mb-8">
+//             <TwitterTweetEmbed
+//               tweetId={'1297199166490206208'}
+//               options={{ dnt: 'true' }}
+//             />
+//           </div>,
+//           <div className="mb-8">
+//             <TwitterTweetEmbed
+//               tweetId={'1328392965484515328'}
+//               options={{ cards: 'hidden', conversation: 'none', dnt: 'true' }}
+//             />
+//           </div>,
+//           <div className="mb-8">
+//             <TwitterTweetEmbed
+//               tweetId={'1321438656809218048'}
+//               options={{ cards: 'hidden', conversation: 'none', dnt: 'true' }}
+//             />
+//           </div>,
+//           <div className="mb-8">
+//             <TwitterTweetEmbed
+//               tweetId={'1294237220623114240'}
+//               options={{ cards: 'hidden', conversation: 'none', dnt: 'true' }}
+//             />
+//           </div>,
+//           <div className="mb-8">
+//             <TwitterTweetEmbed
+//               tweetId={'1275670374668218369'}
+//               options={{ cards: 'hidden', conversation: 'none', dnt: 'true' }}
+//             />
+//           </div>,
+//         ]}
+//       </Masonry>
+//     </>
+//   )
+// }
