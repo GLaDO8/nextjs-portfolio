f48ce7c13b01819f05dbc04352fdc42c807f2417
shreyas gupta <shreyas.charming@gmail.com>
2020-11-26 09:45:10 +0530

    added scroll progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
 import '@/styles/index.css'
+import React, { useEffect, useState } from 'react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [scroll, setScroll] = useState(0)
+
+  useEffect(() => {
+    let progressBarHandler = () => {
+      const totalScroll = document.documentElement.scrollTop
+      const windowHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight
+      const scroll = `${totalScroll / windowHeight}`
+
+      setScroll(scroll)
+    }
+
+    window.addEventListener('scroll', progressBarHandler)
+
+    return () => window.removeEventListener('scroll', progressBarHandler)
+  })
+  return (
+    <>
+      <div className="App">
+        <div className="fixed top-0 left-0 w-full z-10">
+          <div
+            className="bg-black origin-top-left scale-0 h-2"
+            style={{ transform: `scale(${scroll}, 1)` }}
+          />
+        </div>
+      </div>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
