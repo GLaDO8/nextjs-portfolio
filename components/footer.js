d06a329252d1f9a1591e1939289f58f7ff8f557a
shreyas gupta <shreyas.charming@gmail.com>
2020-12-02 11:00:10 +0530

    styling changes

--- components/footer.js
+++ components/footer.js
@@ -2,34 +2,36 @@ import React from 'react'
 import Link from 'next/link'
 export default function Footer() {
   return (
-    <footer>
+    <>
       <hr className="border-accent-2 -mb-16 md:-mb-24 w-11/12 mx-auto" />
-      <div className="md:invisible">
-        <div className="flex justify-center">
-          <div className="text-xs text-inactive-gray max-w-sm font-light">
-            © 2020 by Shreyas Gupta, All rights reserved.
+      <footer>
+        <div className="md:invisible">
+          <div className="flex justify-center">
+            <div className="text-xs text-inactive-gray max-w-sm font-light">
+              © 2020 by Shreyas Gupta, All rights reserved.
+            </div>
+          </div>
+          <div className="flex justify-center flex-1 items-center">
+            <div className="footer-btn py-4 pr-4">Meta</div>
+            <div className="footer-btn py-4 pr-4">
+              <Link href="/thoughtboard">Thought Board</Link>
+            </div>
+            <div className="text-xs footer-btn py-4">Source</div>
           </div>
         </div>
-        <div className="flex justify-center flex-1 items-center">
-          <div className="footer-btn py-4 pr-4">Meta</div>
-          <div className="footer-btn py-4 pr-4">
-            <Link href="/thoughtboard">Thought Board</Link>
+        <div className="items-center w-auto hidden md:flex">
+          <div className="text-sm text-inactive-gray flex justify-start font-light py-4 p-8">
+            © 2020-2077 by Shreyas Gupta, All rights reserved.
           </div>
-          <div className="text-xs footer-btn py-4">Source</div>
-        </div>
-      </div>
-      <div className="items-center w-auto hidden md:flex">
-        <div className="text-sm text-inactive-gray flex justify-start py-4 p-8">
-          © 2020-2077 by Shreyas Gupta, All rights reserved.
-        </div>
-        <div className="flex justify-end flex-1 items-center p-8">
-          <div className="footer-btn py-4 pr-4">META</div>
-          <div className="footer-btn py-4 pr-4">
-            <Link href="/thoughtboard">THOUGHT BOARD</Link>
+          <div className="flex justify-end flex-1 items-center p-8">
+            <div className="footer-btn py-4 pr-4">META</div>
+            <div className="footer-btn py-4 pr-4">
+              <Link href="/thoughtboard">THOUGHT BOARD</Link>
+            </div>
+            <div className="footer-btn py-4">SOURCE</div>
           </div>
-          <div className="footer-btn py-4">SOURCE</div>
         </div>
-      </div>
-    </footer>
+      </footer>
+    </>
   )
 }
--- components/more-stories.js
+++ components/more-stories.js
@@ -2,8 +2,8 @@ import PostPreview from './post-preview'
 
 export default function MoreStories({ posts }) {
   return (
-    <section className="m-16">
-      <div className="grid grid-cols-1 md:grid-cols-3 md:gap-x-16 lg:gap-x-20 gap-y-20 md:gap-y-20 mb-20">
+    <section className="mb-8 lg:m-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-12 lg:gap-x-20 gap-y-12 md:gap-y-20 mb-20">
         {posts.map((post) => (
           <PostPreview
             key={post.slug}
