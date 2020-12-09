b50dcf053392b86d634f0ff7d7b202f33e75ede8
shreyas gupta <shreyas.charming@gmail.com>
2020-12-09 16:35:31 +0530

    updated home page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import NavButton from '@/components/navbutton'
 import Layout from '@/components/layout'
-import Logo from './../public/newlogo.svg'
+import Logo from './../public/logo.svg'
 import Head from 'next/head'
 import Buttonlist from '@/components/buttonlist'
 import MoreStories from '@/components/more-stories'
@@ -13,28 +13,29 @@ export default function Index({ allPosts, allWork }) {
       <Head>
         <title>Home</title>
       </Head>
-      <Logo className="h-20 md:h-24 lg:h-28 mx-auto mt-16" />
-      <div
-        className="w-full lg:flex lg:items-center lg:w-auto hidden pt-6 lg:pt-6"
-        id="nav-content"
-      >
-        <div className="lg:flex justify-center flex-1 items-center">
-          {Buttonlist.map((button) => (
-            <NavButton
-              key={button.path}
-              path={button.path}
-              label={button.label}
-            />
-          ))}
+      <div className=" grid lg:grid-cols-2">
+        <div className="w-1/2 h-1/2">
+          <Logo className="h-20 md:h-24 lg:h-28" />
         </div>
-      </div>
-      <div className="my-24">
-        <div className="text-2xl md:text-3xl lg:text-4xl font-bold mb-8 mx-auto max-w-lg text-center">
-          Hello, netizen.
-        </div>
-        <div className="text-lg md:text-xl lg:text-2xl font-regular mx-auto max-w-xl text-center">
-          I’m Shreyas Gupta, a designer with a knack for building things. Scroll
-          down to view my work.
+        <div>
+          <div
+            className="lg:flex hidden flex-row-reverse pt-6"
+            id="nav-content"
+          >
+            {Buttonlist.map((button) => (
+              <NavButton
+                key={button.path}
+                path={button.path}
+                label={button.label}
+              />
+            ))}
+          </div>
+          <div>
+            <div className="text-base md:text-lg lg:text-xl font-regular z-10 p-8">
+              Hello Netizen, I’m Shreyas Gupta, a designer with a knack for
+              building things.
+            </div>
+          </div>
         </div>
       </div>
 
