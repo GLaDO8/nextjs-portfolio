fbc84e17148c4cbebe8eb1a24537d67a6b39afc5
shreyas gupta <shreyas.charming@gmail.com>
2020-12-20 19:33:57 +0530

    changes to markdown style and social icons

--- components/social-icons.js
+++ components/social-icons.js
@@ -1,26 +1,27 @@
+import Behance from './../public/social/behance.svg'
+import Dribbble from './../public/social/dribbble.svg'
+import Github from './../public/social/github.svg'
+import Instagram from './../public/social/instagram.svg'
+import Twitter from './../public/social/twitter.svg'
+
 export default function Index() {
   return (
     <div className="flex justify-center mb-32 mx-10">
-      <img
-        className="social-icons"
-        src="//s.svgbox.net/social.svg?fill=000000&ic=instagram"
-      />
-      <img
-        className="social-icons"
-        src="//s.svgbox.net/social.svg?fill=000000&ic=github"
-      />
-      <img
-        className="social-icons"
-        src="//s.svgbox.net/social.svg?fill=000000&ic=behance"
-      />
-      <img
-        className="social-icons"
-        src="//s.svgbox.net/social.svg?fill=000000&ic=dribbble"
-      />
-      <img
-        className="social-icons"
-        src="//s.svgbox.net/social.svg?fill=000000&ic=twitter"
-      />
+      <a href="https://dribbble.com/shreyas_gupta">
+        <Dribbble className="social-icons" />
+      </a>
+      <a href="https://github.com/GLaDO8">
+        <Github className="social-icons" />
+      </a>
+      <a href="https://www.behance.net/ShreyasGupta">
+        <Behance className="social-icons" />
+      </a>
+      <a href="https://www.instagram.com/shreyas.jpg/">
+        <Instagram className="social-icons" />
+      </a>
+      <a href="https://twitter.com/shreyasjpg">
+        <Twitter className="social-icons" />
+      </a>
     </div>
   )
 }
