4bf397d906776ad1025e5f873c3970a772d5b45f
shreyas gupta <shreyas.charming@gmail.com>
2020-12-26 18:06:58 +0530

    changes to homepage

--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,20 @@ import Logo from './../public/logo.svg'
 import Head from 'next/head'
 import Buttonlist from '@/components/buttonlist'
 import MoreStories from '@/components/more-stories'
+import WritingList from '@/components/writinglist'
 import SocialIcons from '@/components/social-icons'
 import { getAllPostsForHome, getAllProjectsForHome } from '@/lib/api'
+import { HOME_OG_IMAGE_URL } from '@/lib/constants'
 
 export default function Index({ allPosts, allWork }) {
   return (
     <>
       <Meta />
       <Head>
+        <meta property="og:image" content={HOME_OG_IMAGE_URL} />
         <title>Home</title>
       </Head>
-      <Logo className="h-20 md:h-40 lg:h-56 mx-auto mt-16" />
+      <Logo className="h-48 mx-auto mt-16" />
       <div
         className="flex items-center w-auto"
         id="nav-content"
@@ -30,10 +33,16 @@ export default function Index({ allPosts, allWork }) {
           ))}
         </div>
       </div>
-      <div className="my-24">
-        <div className=" text-base md:text-lg lg:text-xl font-normal mx-auto max-w-md sm:mx-8 md:max-w-xl lg:max-w-2xl text-left">
+      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-20 mb-12 my-16 mx-24">
+        <div >
+          <p className="text-xl font-medium leading-tight md:text-4xl lg:text-5xl mx-16 max-w-sm md:max-w-xl lg:max-w-2xl">
+            Hello Netizen, I’m Shreyas Gupta, a designer with a knack for building things.
+          </p>
+          <SocialIcons/>
+        </div>
+        <div className="text-base md:text-lg lg:text-xl font-normal sm:mx-auto max-w-md md:max-w-xl lg:max-w-2xl text-left mx-4 lg:mx-8">
           <p>
-            Hello netizens, I’m Shreyas Gupta, a design researcher and developer studying at IIIT Bangalore, India. I’m currently working as a Research Intern under Prof. Alexandra Weilenmann at the HCI department at Gothenburg University. Previously I interned with GE Healthcare as a Data Scientist in their services department. I volunteer for EarthHero as an iOS developer, an organisation trying to help people track and reduce their carbon emissions. I was also the organiser for TEDxIIITBangalore’s very first TEDx event, along with my colleague Tejas Kotha.
+            Currently I’m studying at <a href="">IIIT Bangalore</a>, India and working as a Research Intern under Prof. Alexandra Weilenmann at the <a href="">HCI Department</a> at Gothenburg University. Previously I interned with GE Healthcare as a Data Scientist in their Services Department. I volunteer for EarthHero as an iOS Developer, an organisation trying to help people track and reduce their carbon emissions. I was also the organiser for TEDxIIITBangalore’s very first TEDx event, along with my colleague Tejas Kotha.
           </p> 
           <br/>
           <p>
@@ -49,7 +58,7 @@ export default function Index({ allPosts, allWork }) {
       <div className="home-page-title">SELECTED WORK</div>
       <MoreStories posts={allWork} />
       <div className="home-page-title">MY WRITINGS</div>
-      <MoreStories posts={allPosts} />
+      <WritingList posts={allPosts} />
       <div className="flex flex-col mb-24">
         <div className="text-2xl md:text-5xl lg:text-6xl font-bold mx-auto max-w-2xl text-center">
           For work enquiries, freelancing or just a friendly chat,
@@ -58,8 +67,6 @@ export default function Index({ allPosts, allWork }) {
           mail me
         </button>
       </div>
-
-      <SocialIcons />
       <Footer />
     </>
   )
