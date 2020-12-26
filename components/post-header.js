928c9f6d5ce767fa9312c87ac8e428cacbaf8971
shreyas gupta <shreyas.charming@gmail.com>
2020-12-26 18:06:21 +0530

    changed post header order (image on top)

--- components/post-header.js
+++ components/post-header.js
@@ -1,14 +1,15 @@
 import Date from './date'
 import CoverImage from './cover-image'
 import ReadTime from './read-time'
-import ProjectTag from './../public/project-tag.svg'
 
 export default function PostHeader({ title, coverImage, date, excerpt }) {
   return (
     <>
-      <div className="mx-auto max-w-3xl mt-36 lg:mt-16">
-        {/* <ProjectTag className="inline-block h-5 md:h-6 lg:h-10" /> */}
-        <div className="text-xl font-title md:text-5xl lg:text-6xl font-bold tracking-tighter leading-tight md:leading-none sm:mb-0 lg:mb-2">
+      <div className="w-auto mb-8 mt-8 md:mb-12 md:mt-12 lg:mb-16 lg:mt-16">
+        <CoverImage title={title} url={coverImage.imgix_url} />
+      </div>
+      <div className="lg:max-w-2xl max-w-lg mx-auto my-24">
+        <div className="text-2xl font-title md:text-5xl lg:text-6xl font-bold tracking-tighter leading-tight md:leading-none sm:mb-0 lg:mb-2">
           <h1>{title}</h1>
         </div>
         <p className="text-base md:text-xl lg:text-xl mb-4 leading-relaxed lg:leading-relaxed mt-4">
@@ -18,9 +19,6 @@ export default function PostHeader({ title, coverImage, date, excerpt }) {
           <Date dateString={date} />
         </div>
         {/* <div>{post.minutes}</div> */}
-      </div>
-      <div className="w-auto mb-8 md:mb-12 lg:mb-16">
-        <CoverImage title={title} url={coverImage.imgix_url} />
       </div>
     </>
   )
