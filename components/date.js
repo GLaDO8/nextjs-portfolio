0e51a02a306c94dc54d321c1f3676fea9376ade2
shreyas gupta <shreyas.charming@gmail.com>
2021-01-03 18:28:44 +0530

    new date format

--- components/date.js
+++ components/date.js
@@ -5,8 +5,6 @@ export default function Date({ dateString }) {
   return (
     <div>
       <time dateTime={dateString}>{format(date, "d LLL, yyyy")}</time>
-      <span> at </span>
-      <time dateTime={dateString}>{format(date, "H:m")}</time>
     </div>
   );
 }
