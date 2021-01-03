fb2a77940860ec3d25ab8a3fd76f5b7d8d2ea2af
shreyas gupta <shreyas.charming@gmail.com>
2021-01-03 18:27:25 +0530

    changed navbar

--- components/navbar.js
+++ components/navbar.js
@@ -3,39 +3,26 @@ import Link from 'next/link'
 import Logo from './../public/logo.svg'
 const Navbar = (props) => (
   <>
-    <nav className="hidden md:flex justify-between bg-white p-2 lg:p-6 z-10 mb-2 md:mb-4 lg:mb-6">
-      <div className="flex mr-2 lg:mr-6">
-        <Link href="/">
-          <a>
-            <Logo className="h-10 md:h-12 lg:h-16" />
-          </a>
-        </Link>
-      </div>
-      <div
-        className="md:flex hidden justify-end flex-1 items-center"
-        id="nav-content"
-      >
-        {props.navButtons.map((button) => (
-          <NavButton
-            key={button.path}
-            path={button.path}
-            label={button.label}
-          />
-        ))}
-      </div>
-    </nav>
-
-    <nav>
-      <div className="flex md:hidden my-4">
-        <div className="flex-1">
+    <div className="container mx-auto max-w-3xl">
+      <nav className="flex justify-between mt-8 z-10 mb-2 md:mb-4 lg:mb-6">
+        <div>
           <Link href="/">
             <a>
-              <Logo className="h-10" />
+              <Logo className="h-14 w-14 md:h-16 md:w-16 lg:h-18 lg:w-18" />
             </a>
           </Link>
         </div>
-      </div>
-    </nav>
+        <div className="flex flex-row items-center" id="nav-content">
+          {props.navButtons.map((button) => (
+            <NavButton
+              key={button.path}
+              path={button.path}
+              label={button.label}
+            />
+          ))}
+        </div>
+      </nav>
+    </div>
   </>
 )
 
