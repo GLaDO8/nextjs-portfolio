9c32bce4307e5f36d333f6116e691fb65662ac1e
shreyas gupta <shreyas.charming@gmail.com>
2021-01-07 09:40:49 +0530

    removed custom fonts

--- components/social-icons.js
+++ components/social-icons.js
@@ -1,20 +1,35 @@
 export default function Index() {
   return (
-    <div className="hidden md:flex justify-start mb-32 mx-10">
+    <div className="flex justify-center max-w-3xl mx-auto">
       <a href="https://dribbble.com/shreyas_gupta">
-        <img className="social-icons" src="https://s.svgbox.net/social.svg?fill=000000&ic=dribbble"/>
+        <img
+          className="social-icons"
+          src="https://s.svgbox.net/social.svg?fill=000000&ic=dribbble"
+        />
       </a>
       <a href="https://github.com/GLaDO8">
-        <img className="social-icons" src="https://s.svgbox.net/social.svg?fill=000000&ic=github"/>
+        <img
+          className="social-icons"
+          src="https://s.svgbox.net/social.svg?fill=000000&ic=github"
+        />
       </a>
       <a href="https://www.behance.net/ShreyasGupta">
-        <img className="social-icons" src="https://s.svgbox.net/social.svg?fill=000000&ic=behance"/>
+        <img
+          className="social-icons"
+          src="https://s.svgbox.net/social.svg?fill=000000&ic=behance"
+        />
       </a>
       <a href="https://www.instagram.com/shreyas.jpg/">
-        <img className="social-icons" src="https://s.svgbox.net/social.svg?fill=000000&ic=instagram"/>
+        <img
+          className="social-icons"
+          src="https://s.svgbox.net/social.svg?fill=000000&ic=instagram"
+        />
       </a>
       <a href="https://twitter.com/shreyasjpg">
-        <img className="social-icons" src="https://s.svgbox.net/social.svg?fill=000000&ic=twitter"/>
+        <img
+          className="social-icons"
+          src="https://s.svgbox.net/social.svg?fill=000000&ic=twitter"
+        />
       </a>
     </div>
   )
