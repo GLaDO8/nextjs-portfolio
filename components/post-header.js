409a99236e05b69284954fa08ffe52e00b6993fb
shreyas gupta <shreyas.charming@gmail.com>
2021-01-17 08:22:51 +0530

    updated post header

--- components/post-header.js
+++ components/post-header.js
@@ -10,13 +10,13 @@ export default function PostHeader({ title, coverImage, date, excerpt }) {
       </div>
       <div className="mx-4 md:mx-8 lg:mx-12">
         <div className="max-w-3xl mx-auto mt-6 lg:mt-12">
-          <div className="text-2xl font-title md:text-5xl lg:text-6xl font-bold tracking-tighter leading-tight md:leading-none sm:mb-0 lg:mb-2">
+          <div className="text-2xl font-title md:text-5xl lg:text-6xl font-bold tracking-tighter leading-tight md:leading-none mb-2">
             <h1>{title}</h1>
           </div>
-          <p className="text-base md:text-xl lg:text-xl mb-4 leading-relaxed mt-4">
+          <p className="text-base md:text-xl lg:text-xl mb-2 leading-relaxed italic font-light">
             {excerpt}
           </p>
-          <div className="mb-6 text-base md:text-lg lg:text-xl text-grey-secondary font-light">
+          <div className="mb-8 text-base md:text-lg lg:text-lg text-grey-secondary font-light">
             <Date dateString={date} />
           </div>
           {/* <div>{post.minutes}</div> */}
