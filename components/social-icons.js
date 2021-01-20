dc18c793f9df363b701929b92d6fd405da36fd47
shreyas gupta <shreyas.charming@gmail.com>
2021-01-20 19:30:59 +0530

    changes to social icons

--- components/social-icons.js
+++ components/social-icons.js
@@ -1,34 +1,39 @@
 export default function Index() {
   return (
-    <div className="flex justify-center max-w-3xl mx-auto">
+    <div className="flex max-w-3xl my-8">
       <a href="https://dribbble.com/shreyas_gupta">
         <img
           className="social-icons"
           src="https://s.svgbox.net/social.svg?fill=000000&ic=dribbble"
+          alt="link to Dribbble profile"
         />
       </a>
       <a href="https://github.com/GLaDO8">
         <img
           className="social-icons"
           src="https://s.svgbox.net/social.svg?fill=000000&ic=github"
-        />
-      </a>
-      <a href="https://www.behance.net/ShreyasGupta">
-        <img
-          className="social-icons"
-          src="https://s.svgbox.net/social.svg?fill=000000&ic=behance"
+          alt="link to Github profile"
         />
       </a>
       <a href="https://www.instagram.com/shreyas.jpg/">
         <img
           className="social-icons"
           src="https://s.svgbox.net/social.svg?fill=000000&ic=instagram"
+          alt="link to Instagram profile"
         />
       </a>
       <a href="https://twitter.com/shreyasjpg">
         <img
           className="social-icons"
           src="https://s.svgbox.net/social.svg?fill=000000&ic=twitter"
+          alt="link to Twitter profile"
+        />
+      </a>
+      <a href="https://www.behance.net/ShreyasGupta">
+        <img
+          className="social-icons"
+          src="https://s.svgbox.net/social.svg?fill=000000&ic=behance"
+          alt="link to Behance profile"
         />
       </a>
     </div>
