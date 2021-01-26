8e68e2c7ebb0acdd02190b13df8ace9a86a2a1d7
shreyas gupta <shreyas.charming@gmail.com>
2021-01-26 10:59:37 +0530

    changed writing previews to grid layout

--- components/writing-preview.js
+++ components/writing-preview.js
@@ -1,10 +1,26 @@
 import Link from 'next/link'
-export default function writingPreview({ title, udate, excerpt, slug }) {
+import Image from 'next/image'
+export default function writingPreview({
+  coverImage,
+  title,
+  udate,
+  excerpt,
+  slug,
+}) {
   return (
     <>
       <Link as={`/writings/${slug}`} href="/writings/[slug]">
-        <div className="flex cursor-pointer mb-6">
-          <div className="flex-initial">
+        <div className="cursor-pointer mb-6">
+          <div className="transform hover:shadow-2xl duration-500 rounded-xl overflow-hidden cursor-pointer">
+            <Image
+              src={coverImage.imgix_url}
+              alt={`Cover Image for ${title}`}
+              width={1920}
+              height={1080}
+              layout="responsive"
+            />
+          </div>
+          <div className="mt-4">
             <h3 className="hover:underline text-lg lg:text-xl mb-2 font-semibold leading-snug text-black">
               {title}
             </h3>
--- components/writinglist.js
+++ components/writinglist.js
@@ -3,9 +3,10 @@ import WritingPreview from './writing-preview'
 export default function WritingsList({ posts }) {
   return (
     <section>
-      <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         {posts.map((post) => (
           <WritingPreview
+            coverImage={post.metadata.cover_image}
             key={post.slug}
             title={post.title}
             udate={post.metadata.udate}
