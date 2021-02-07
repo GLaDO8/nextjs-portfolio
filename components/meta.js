e4504a66339880c8ab1a244cd4b47be7306ef2b7
shreyas gupta <shreyas.charming@gmail.com>
2021-02-07 21:14:28 +0530

    website is now index-able

--- components/meta.js
+++ components/meta.js
@@ -82,6 +82,7 @@ export default function Meta() {
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
+      <meta name="robots" content="index, follow" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
     </Head>
   )
