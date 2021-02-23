7918cf8c711bae35a089dd56cffe5d7a77e5384e
shreyas gupta <shreyas.charming@gmail.com>
2021-02-23 06:30:46 +0530

    removed empty sitemap, dark mode headings

--- components/post-body.js
+++ components/post-body.js
@@ -4,7 +4,7 @@ import markdownStyles from './markdown-styles.module.css'
 
 export default function PostBody({ content }) {
   return (
-    <div className="mx-4 md:mx-8 lg:mx-12">
+    <div className="mx-4 md:mx-8 lg:mx-12 text-black dark:text-white">
       <div
         className={markdownStyles['markdown']}
         dangerouslySetInnerHTML={{ __html: content }}
