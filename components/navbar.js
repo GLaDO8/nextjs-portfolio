77771ec9b83b464140cf118064726e39b1fc4866
shreyas gupta <shreyas.charming@gmail.com>
2021-02-28 20:04:44 +0530

    updated navbar dark mode toggle, logo hover

--- components/navbar.js
+++ components/navbar.js
@@ -22,12 +22,12 @@ function Navbar({ navButtons }) {
             <Link href="/">
               <a>
                 <Logo
-                  className={`h-14 w-14 md:h-16 md:w-16 lg:h-18 lg:w-18 ${
+                  className={`h-14 w-14 md:h-16 md:w-16 lg:h-18 lg:w-18 hover:opacity-80 transition duration-300 ${
                     theme === 'dark' ? 'hidden' : 'flex-1'
                   }`}
                 />
                 <DarkLogo
-                  className={`h-14 w-14 md:h-16 md:w-16 lg:h-18 lg:w-18 ${
+                  className={`h-14 w-14 md:h-16 md:w-16 lg:h-18 lg:w-18 hover:opacity-80 transition duration-300 ${
                     theme === 'dark' ? 'flex-1' : 'hidden'
                   }`}
                 />
@@ -43,26 +43,36 @@ function Navbar({ navButtons }) {
               />
             ))}
             <button
-              className="p-2 md:p-3 text-white dark:text-black bg-gray-300 dark:bg-gray-50 bg-opacity-50 font-semibold rounded-md ml-4"
-              onClick={() => {
-                setTheme(theme === 'light' ? 'dark' : 'light')
-              }}
-              aria-label="dark mode toggle"
+              aria-label="Toggle Dark Mode"
+              type="button"
+              className="bg-gray-300 dark:bg-gray-50 rounded-lg p-3 h-10 w-10 ml-4"
+              onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
             >
-              <img
-                src="https://s2.svgbox.net/hero-solid.svg?ic=moon&color=000"
-                className={`h-4 w-4 md:h-5 md:w-5 lg:h-6 lg:w-6 transition-all duration-500 ${
-                  theme === 'dark' ? 'hidden' : ''
-                }`}
-                alt="dark mode toggle - switch to light mode"
-              />
-              <img
-                src="https://s2.svgbox.net/hero-solid.svg?ic=sun&color=000"
-                className={`h-4 w-4 md:h-5 md:w-5 lg:h-6 lg:w-6 transition-all duration-500 ${
-                  theme === 'dark' ? '' : 'hidden'
-                }`}
-                alt="dark mode toggle - switch to dark mode"
-              />
+              {mounted && (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  stroke="currentColor"
+                  className="h-4 w-4 text-black"
+                >
+                  {theme === 'dark' ? (
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                    />
+                  ) : (
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                    />
+                  )}
+                </svg>
+              )}
             </button>
           </div>
         </nav>
