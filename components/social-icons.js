9d7985d79c8730c8cb0436326180875b68906650
shreyas gupta <shreyas.charming@gmail.com>
2021-04-29 12:09:00 +0530

    simplified social icon link generation

--- components/social-icons.js
+++ components/social-icons.js
@@ -9,78 +9,40 @@ export default function Index() {
 
   if (!mounted) return null
 
-  const twitterSvgTagLink = `https://s2.svgbox.net/social.svg?ic=twitter&color=${
-    theme === 'light' ? '000' : 'fff'
-  }`
-  const dribbbleSvgTagLink = `https://s2.svgbox.net/social.svg?ic=dribbble&color=${
-    theme === 'light' ? '000' : 'fff'
-  }`
-  const githubSvgTagLink = `https://s2.svgbox.net/social.svg?ic=github&color=${
-    theme === 'light' ? '000' : 'fff'
-  }`
-  const instagramSvgTagLink = `https://s2.svgbox.net/social.svg?ic=instagram&color=${
-    theme === 'light' ? '000' : 'fff'
-  }`
-  const behanceSvgTagLink = `https://s2.svgbox.net/social.svg?ic=behance&color=${
-    theme === 'light' ? '000' : 'fff'
-  }`
+  var linkList = [
+    'https://www.linkedin.com/in/shreyas-gupta/',
+    'https://dribbble.com/shreyasgupta',
+    'https://github.com/GLaDO8',
+    'https://www.instagram.com/shreyas.jpg/',
+    'https://twitter.com/shreyasjpg',
+    'https://www.behance.net/ShreyasGupta',
+  ]
+  var iconList = [
+    'linkedin',
+    'dribbble',
+    'github',
+    'instagram',
+    'twitter',
+    'behance',
+  ]
+
+  function iconLinkGenerator(name, theme, link) {
+    return (
+      <a href={link} target="_blank" rel="noreferrer noopener">
+        <img
+          src={`https://s2.svgbox.net/social.svg?ic=${name}&color=${
+            theme === 'light' ? '000' : 'fff'
+          }`}
+          className="social-icons"
+        />
+      </a>
+    )
+  }
   return (
     <div className="flex max-w-3xl my-8">
-      <a
-        href="https://dribbble.com/shreyasgupta"
-        target="_blank"
-        rel="noreferrer noopener"
-      >
-        <img
-          className="social-icons"
-          src={dribbbleSvgTagLink}
-          alt="link to Dribbble profile"
-        />
-      </a>
-      <a
-        href="https://github.com/GLaDO8"
-        target="_blank"
-        rel="noreferrer noopener"
-      >
-        <img
-          className="social-icons"
-          src={githubSvgTagLink}
-          alt="link to Github profile"
-        />
-      </a>
-      <a
-        href="https://www.instagram.com/shreyas.jpg/"
-        target="_blank"
-        rel="noreferrer noopener"
-      >
-        <img
-          className="social-icons"
-          src={instagramSvgTagLink}
-          alt="link to Instagram profile"
-        />
-      </a>
-      <a
-        href="https://twitter.com/shreyasjpg"
-        target="_blank"
-        rel="noreferrer noopener"
-      >
-        <img
-          className="social-icons"
-          src={twitterSvgTagLink}
-          alt="link to Twitter profile"
-        />
-      </a>
-      <a
-        href="https://www.behance.net/ShreyasGupta"
-        target="_blank"
-        rel="noreferrer noopener"
-      >
-        <img
-          className="social-icons"
-          src={behanceSvgTagLink}
-          alt="link to Behance profile"
-        />
-      </a>
+      {linkList.map((link, index) =>
+        iconLinkGenerator(iconList[index], theme, link),
+      )}
     </div>
   )
 }
