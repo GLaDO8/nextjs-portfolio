f976dcc03c2d6562783cee14000a761f46e35a84
shreyas gupta <shreyas.charming@gmail.com>
2021-05-03 09:32:31 +0530

    fix navbar active link styling

--- components/navbutton.js
+++ components/navbutton.js
@@ -1,11 +1,14 @@
 import Link from 'next/link'
-import { withRouter } from 'next/router'
+import { withRouter } from 'next/router' //withRouter adds the router prop to the const
 
+//use the router prop to get the pathname, split it on '/' and style the link by giving it a classname which matches the base path (/writing, /portfolio)
 const NavButton = (props) => (
-  <Link href={props.path} passHref={true}>
+  <Link href={props.path}>
     <div
       className={`menu-btn-lg cursor-pointer ${
-        props.router.pathname === props.path ? 'menu-btn-lg-active' : ''
+        `/${props.router.pathname.split('/')[1]}` === props.path
+          ? 'menu-btn-lg-active'
+          : ''
       }`}
     >
       {props.label}
