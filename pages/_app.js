5a60ab7e5dae2500b3bf68b30ceaa992e4a63adf
shreyas gupta <shreyas.charming@gmail.com>
2021-07-18 23:26:41 +0530

    updated _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,11 @@
 import '@/styles/index.css'
 import { ThemeProvider } from 'next-themes'
-import PlausibleProvider from 'next-plausible'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <PlausibleProvider domain="shreyas.design">
-      <ThemeProvider
-        forcedTheme={Component.theme || undefined}
-        attribute="class"
-      >
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </PlausibleProvider>
+    <ThemeProvider forcedTheme={Component.theme || undefined} attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
   )
 }
 
