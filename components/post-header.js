61e8278a1545dfd3e61e70f4004b0fcef6ae6b48
GLaDO8 <shreyas.charming@gmail.com>
2022-10-26 15:16:03 +0530

    responsive post header fixes

--- components/post-header.js
+++ components/post-header.js
@@ -13,11 +13,11 @@ export default function PostHeader({
 
   return (
     <>
-      <div className="mx-4 md:mx-8 lg:mx-12">
-        <div className="max-w-3xl mx-auto mt-6 lg:mt-12">
+      <div className="mx-4 md:mx-12">
+        <div className="max-w-3xl mx-auto mt-8 lg:mt-12">
           <div className="flex flex-row mr-4 text-sm font-normal text-black sm:text-base dark:text-white">
             <Date dateString={date} />
-            <div className="mx-4 mt-2">
+            <div className="mx-2 mt-2">
               <img src={timeLink} width="6" height="6" aria-hidden="true" />
             </div>
             <div className="mb-2 text-sm font-normal text-black sm:text-base dark:text-white">
@@ -27,12 +27,12 @@ export default function PostHeader({
           <div className="mb-2 text-2xl font-bold leading-tight tracking-tighter text-black font-title md:text-5xl lg:text-7xl md:leading-none dark:text-white">
             <h1>{title}</h1>
           </div>
-          <p className="mb-2 text-lg leading-relaxed text-black font-regular dark:text-white">
+          <p className="mb-2 leading-relaxed text-black md:text-lg font-regular dark:text-white">
             {excerpt}
           </p>
         </div>
       </div>
-      <div className="w-auto my-8 mb-24">
+      <div className="w-auto my-8 mb-8 md:mb-16 lg:mb-24">
         <CoverImage title={title} url={coverImage.imgix_url} />
       </div>
     </>
