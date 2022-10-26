178ded9c593b4871fa5d1c067dfaa9a24e3ef06f
GLaDO8 <shreyas.charming@gmail.com>
2022-10-26 15:16:19 +0530

    responsive writings preview fixes

--- components/writing-preview.js
+++ components/writing-preview.js
@@ -1,22 +1,26 @@
 import Link from 'next/link'
-import Image from 'next/image'
-export default function writingPreview({
-  coverImage,
-  title,
-  udate,
-  excerpt,
-  slug,
-}) {
+export default function writingPreview({ title, udate, excerpt, slug }) {
   return (
     <>
       <Link as={`/writings/${slug}`} href="/writings/[slug]">
-        <div className="mb-4 cursor-pointer group">
-          <div className="mt-4">
-            <h4 className="mb-2 font-semibold leading-snug text-white transition duration-500 transform group-hover:underline">
-              {title}
-            </h4>
-            <p className="mb-2 leading-snug">{excerpt}</p>
-            <p className=" text-grey-secondary">{udate}</p>
+        <div className="">
+          <div className="hidden gap-4 my-4 cursor-pointer md:flex-row md:flex group">
+            <p className="w-24 text-right text-grey-secondary">{udate}</p>
+            <div>
+              <h4 className="mb-2 font-semibold leading-snug text-white transition duration-500 transform group-hover:underline">
+                {title}
+              </h4>
+              <p className="mb-2 leading-snug">{excerpt}</p>
+            </div>
+          </div>
+          <div className="gap-4 my-4 cursor-pointer md:hidden group">
+            <div>
+              <h4 className="mb-2 font-semibold leading-snug text-white transition duration-500 transform group-hover:underline">
+                {title}
+              </h4>
+              <p className="mb-2 leading-snug">{excerpt}</p>
+              <p className=" text-grey-secondary">{udate}</p>
+            </div>
           </div>
         </div>
       </Link>
