d009506c8eac609e3a1ee5b71e3fe8fc98043816
GLaDO8 <shreyas.charming@gmail.com>
2022-10-27 09:20:04 +0530

    added new content layout

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,92 @@ import Link from 'next/link'
 import Image from 'next/image'
 import ProjectList from '@/components/projectlist'
 import WritingList from '@/components/writinglist'
-import { getAllPostsForHome, getAllProjectsForHome } from '@/lib/api'
+import { getAllPosts, getAllProjectsForHome } from '@/lib/api'
 import { HOME_OG_IMAGE_URL } from '@/lib/constants'
 import Layout from '@/components/layout'
 
 export default function Index({ allPosts, allWork }) {
+  const shortAboutText = (
+    <p>
+      Hi, I'm Shrey, a design engineer who loves building tools for
+      productivity. Currently building design systems, processes and a home for
+      data teams at {darkModeLinkStyleTag('Atlan', 'https://atlan.com/')}.
+      <br />
+      <br />
+      In the past, I've built tools to study technology use in families. I have
+      two cats, love talking about sustainable lifestyles, a huge fan of{' '}
+      {darkModeLinkStyleTag(
+        'Khruangbin',
+        'https://www.youtube.com/watch?v=q4xKvHANqjk',
+      )}{' '}
+      and a lifelong stan of the{' '}
+      {darkModeLinkStyleTag(
+        'Mass Effect universe',
+        'https://en.wikipedia.org/wiki/Mass_Effect',
+      )}
+      .
+    </p>
+  )
+
+  const longAboutText = (
+    <div>
+      <p>
+        Hi, I'm Shrey, a design engineer who loves building tools for
+        productivity. Currently building design systems, processes and a home
+        for data teams at {darkModeLinkStyleTag('Atlan', 'https://atlan.com/')}.
+        <br />
+        <br />
+        At my alma mater{' '}
+        {darkModeLinkStyleTag('IIIT Bangalore', 'https://www.iiitb.ac.in/')}, I
+        studied electronics, then explored to data science for a while which
+        landed me an internship at{' '}
+        {darkModeLinkStyleTag('GE Healthcare', 'https://www.gehealthcare.in/')}
+        's data team in the services department. The work was great, we were
+        building a process for the services team to optimise the sales of
+        refurbished equipment, which has high demand in third world countries.
+        But I wanted to study human behaviour,
+        <br />
+        <br />
+        During this time I was also taking various courses like Human-computer
+        interaction, qualitative research methods and Web and the Mind. These
+        courses made me gravitate towards HCI and product design, and I started
+        working with the HCI department at the{' '}
+        {darkModeLinkStyleTag(
+          'University of Gothenburg',
+          'https://www.gu.se/en',
+        )}
+        . There, I built methodologies and tools to better study technology use
+        in families as part of my masters thesis under{' '}
+        {darkModeLinkStyleTag(
+          'Prof. Mattias Rost',
+          'https://www.gu.se/en/about/find-staff/mattiasrost',
+        )}
+        .
+        <br />
+        <br />I have two cats, Ginger and Loki. I will do anything for them,
+        they're my lifelines. I also advocate leading a sustainable lifestyle,
+        and a huge fan of{' '}
+        {darkModeLinkStyleTag(
+          'Khruangbin',
+          'https://www.youtube.com/watch?v=q4xKvHANqjk',
+        )}{' '}
+        and a lifelong stan of the{' '}
+        {darkModeLinkStyleTag(
+          'Mass Effect universe',
+          'https://en.wikipedia.org/wiki/Mass_Effect',
+        )}
+        .
+      </p>
+    </div>
+  )
+  const [aboutType, setAboutType] = useState('short')
+  function aboutToggle(aboutType) {
+    if (aboutType === 'short') {
+      setAboutType('long')
+    } else {
+      setAboutType('short')
+    }
+  }
   const twitterSvgTagLink = `https://s2.svgbox.net/social.svg?ic=twitter&color=000`
 
   //dark mode link styling for links
@@ -53,35 +134,11 @@ export default function Index({ allPosts, allWork }) {
         {/* <Fade bottom> */}
         <div className="max-w-lg mx-auto mt-6 mb-12 lg:mt-24">
           <p className="mt-8 text-left">
-            I'm Shreyas Gupta, a design engineer who loves building tools for
-            productivity. Currently building design systems, processes and a
-            home for data teams at{' '}
-            {darkModeLinkStyleTag('Atlan', 'https://atlan.com/')}.
-            <br />
-            <br />
-            In the past, I've built tools to study technology use in families. I
-            have two cats, love talking about sustainable lifestyles, a huge fan
-            of{' '}
-            {darkModeLinkStyleTag(
-              'Khruangbin',
-              'https://www.youtube.com/watch?v=q4xKvHANqjk',
-            )}{' '}
-            and a lifelong stan of the{' '}
-            {darkModeLinkStyleTag(
-              'Mass Effect universe',
-              'https://en.wikipedia.org/wiki/Mass_Effect',
-            )}
-            .
+            {aboutType === 'short' ? shortAboutText : longAboutText}
           </p>
-          <div className="flex mt-2">
-            {/* <Link href="/about" passHref={true}>
-              <button className="px-4 py-2 mt-4 mr-4 font-normal text-white transition duration-300 transform bg-black border-2 border-black cursor-pointer dark:bg-white dark:border-white dark:text-black rounded-xl hover:opacity-80">
-                Know More
-              </button>
-            </Link> */}
-
+          <div className="flex content-center justify-between mt-6 md:pr-12">
             <Link href="https://twitter.com/wutamelonshrey" passHref={true}>
-              <button className="flex items-center px-3 py-1 mt-4 text-black transition duration-300 transform bg-white rounded-lg cursor-pointer hover:opacity-70">
+              <button className="flex items-center px-3 py-1 text-black transition duration-300 transform bg-white rounded-lg cursor-pointer hover:opacity-70">
                 <div>
                   <img
                     src={twitterSvgTagLink}
@@ -93,130 +150,157 @@ export default function Index({ allPosts, allWork }) {
                 <div className="ml-2 font-medium"> Say hi on bird app.</div>
               </button>
             </Link>
-          </div>
-        </div>
-        <div class="flex flex-col md:flex-row items-center p-4 border-gray-200 dark:border-white border-2 rounded-xl mt-24 md:mt-36 lg:mt-48">
-          <div>
-            <p class="font-semibold">New extension!</p>
-            <p>
-              Control your Pi-hole from Raycast. From viewing query logs and
-              Pi-hole health to updating blocklists and remotely disabling it.{' '}
-            </p>
-          </div>
-          <div class="mt-4 md:mt-0 md:ml-12 cursor-pointer flex items-center">
-            <Link
-              target="_blank"
-              rel="noopener noreferrer"
-              title="Install Pie for Pi-hole Raycast Extension"
-              href="https://www.raycast.com/GLaDO8/pie-for-pi-hole#install"
+            <button
+              className="underline"
+              onClick={() => aboutToggle(aboutType)}
             >
-              <Image
-                width={256}
-                height={64}
-                src="/images/install_button@2x.png"
-                layout="fixed"
-              />
-            </Link>
+              {aboutType === 'short' ? 'Read long intro' : 'Read short intro'}
+            </button>
           </div>
         </div>
+
         <div className="mt-16">
           <div className="home-page-title">My Work</div>
-          <div>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://glado8.notion.site/Designing-a-Marketplace-for-Notion-8ba5abea314e4620800d282762fcb024"
-            >
-              <div className="mb-12 group">
-                <div className="overflow-hidden duration-500 transform cursor-pointer group-hover:shadow-xl hover:scale-100 rounded-xl">
+          <div className="grid grid-cols-8 gap-4">
+            <div className="flex flex-col col-span-8 lg:col-span-5 gap-y-4">
+              <div>
+                <a
+                  className="hover:border-4"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://glado8.notion.site/Designing-a-Marketplace-for-Notion-8ba5abea314e4620800d282762fcb024"
+                >
+                  <div className="group">
+                    <div className="overflow-hidden duration-500 transform cursor-pointer group-hover:shadow-xl rounded-xl">
+                      <Image
+                        className="relative object-cover w-full"
+                        src="/images/notion.png"
+                        alt="Cover image for playground section"
+                        width={1600}
+                        height={900}
+                        layout="responsive"
+                      />
+                    </div>
+                  </div>
+                </a>
+              </div>
+
+              <a
+                href="https://www.raycast.com/GLaDO8/pie-for-pi-hole#install"
+                className="flex lg:hidden"
+              >
+                <div className="overflow-hidden rounded-xl bg-slate-800">
                   <Image
-                    src="/images/notion-marketplace.png"
-                    alt="Cover image for playground section"
-                    width={1600}
-                    height={900}
+                    className="relative object-cover"
+                    src="/images/pie-long.svg"
+                    alt="Pie for Pi-hole, a raycast extension"
+                    width={620}
+                    height={350}
                     layout="responsive"
                   />
                 </div>
-                <div className="mt-4">
-                  <h3 className="mb-2 text-lg font-semibold leading-snug text-black group-hover:underline lg:text-xl dark:text-white">
-                    Designing a Marketplace for Notion
-                  </h3>
-                  <p className="mb-2 text-base font-normal leading-snug text-black cursor-pointer lg:text-lg dark:text-white">
-                    A case study on how would one go about building a
-                    marketplace for Notion, made in Notion! Created as part of
-                    Atlan's design task.
-                  </p>
-                </div>
+              </a>
+              <ProjectList posts={allWork} />
+              <div>
+                <a
+                  className="hover:border-4"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://jstrieb.github.io/link-lock/#eyJ2IjoiMC4wLjEiLCJlIjoiU3B3c2s5TUVsQzluWTJCRk9oOFpXZzB0TkM3VWJrZGZSSnhMeTdncllqYm1RMHVYZzIrMFEwN0pJd3BsYXdYeWJ1blczWTFoMnFzb2RUaDN1TGIzRklRV0FDWjZ6c2xvRDlHVWM3Qkw2MTFXNjdMMzdyU3dUUT09IiwiaSI6IlE3LzZiRHpyUENRanAwREQifQ=="
+                >
+                  <div className="group">
+                    <div className="overflow-hidden duration-500 transform cursor-pointer group-hover:shadow-xl rounded-xl">
+                      <Image
+                        className="relative object-cover w-full"
+                        src="/images/atlan.png"
+                        alt="Cover image for playground section"
+                        width={1600}
+                        height={900}
+                        layout="responsive"
+                      />
+                    </div>
+                  </div>
+                </a>
               </div>
-            </a>
+
+              <a
+                href="https://glado8.notion.site/glado8/An-Amateur-s-Guide-to-Leading-a-Sustainable-Lifestyle-in-India-b5b80a9e97c24ee0bf9c660f75ea9fba"
+                className="flex lg:hidden"
+              >
+                <div className="overflow-hidden rounded-xl bg-slate-800">
+                  <Image
+                    className="relative object-cover"
+                    src="/images/sustain.svg"
+                    alt="Guide to leading a sustainable lifestyle in india"
+                    width={620}
+                    height={350}
+                    layout="responsive"
+                  />
+                </div>
+              </a>
+
+              <a
+                href="https://shreyas.design/writings/dissecting-my-workflow"
+                className="flex lg:hidden"
+              >
+                <div className="overflow-hidden rounded-xl bg-slate-800">
+                  <Image
+                    className="relative object-cover"
+                    src="/images/tools-long.png"
+                    alt="Article about tools I use"
+                    width={620}
+                    height={302}
+                    layout="responsive"
+                  />
+                </div>
+              </a>
+            </div>
+            <div className="hidden col-span-3 gap-4 lg:flex lg:flex-col">
+              <a href="https://www.raycast.com/GLaDO8/pie-for-pi-hole#install">
+                <div className="overflow-hidden rounded-xl bg-slate-800">
+                  <Image
+                    className="relative object-cover"
+                    src="/images/pie.svg"
+                    alt="Pie for Pi-hole, a raycast extension"
+                    width={360}
+                    height={493}
+                    layout="responsive"
+                  />
+                </div>
+              </a>
+
+              <a href="https://glado8.notion.site/glado8/An-Amateur-s-Guide-to-Leading-a-Sustainable-Lifestyle-in-India-b5b80a9e97c24ee0bf9c660f75ea9fba">
+                <div className="overflow-hidden rounded-xl bg-slate-800">
+                  <Image
+                    className="relative object-cover"
+                    src="/images/sustain-long.svg"
+                    alt="Guide to leading a sustainable lifestyle in india"
+                    width={360}
+                    height={493}
+                    layout="responsive"
+                  />
+                </div>
+              </a>
+
+              <a href="https://shreyas.design/writings/dissecting-my-workflow">
+                <div className="overflow-hidden rounded-xl bg-slate-800">
+                  <Image
+                    className="relative object-cover"
+                    src="/images/tools.svg"
+                    alt="Article about tools I use"
+                    width={360}
+                    height={302}
+                    layout="responsive"
+                  />
+                </div>
+              </a>
+            </div>
           </div>
-          <ProjectList posts={allWork} />
         </div>
 
-        <div>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://glado8.notion.site/An-Amateur-s-Guide-to-Leading-a-Sustainable-Life-in-India-b5b80a9e97c24ee0bf9c660f75ea9fba"
-          >
-            <div className="group">
-              <div className="overflow-hidden duration-500 transform cursor-pointer group-hover:shadow-xl hover:scale-100 rounded-xl">
-                <Image
-                  src="/images/sustainable.png"
-                  alt="Cover image for playground section"
-                  width={1600}
-                  height={900}
-                  layout="responsive"
-                />
-              </div>
-              <div className="mt-4">
-                <h3 className="mb-2 text-lg font-semibold leading-snug text-black group-hover:underline lg:text-xl dark:text-white">
-                  Guide to leading a sustainable lifestyle
-                </h3>
-                <p className="mb-2 text-base font-normal leading-snug text-black cursor-pointer lg:text-lg dark:text-white">
-                  A simple guide leading a sustainable lifestyle, tailored for
-                  Indians.
-                </p>
-              </div>
-            </div>
-          </a>
-        </div>
-        <div className="mt-16">
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://jstrieb.github.io/link-lock/#eyJ2IjoiMC4wLjEiLCJlIjoiU3B3c2s5TUVsQzluWTJCRk9oOFpXZzB0TkM3VWJrZGZSSnhMeTdncllqYm1RMHVYZzIrMFEwN0pJd3BsYXdYeWJ1blczWTFoMnFzb2RUaDN1TGIzRklRV0FDWjZ6c2xvRDlHVWM3Qkw2MTFXNjdMMzdyU3dUUT09IiwiaSI6IlE3LzZiRHpyUENRanAwREQifQ=="
-          >
-            <div className="group">
-              <div className="relative overflow-hidden duration-500 transform cursor-pointer group-hover:shadow-xl hover:scale-100 rounded-xl">
-                <Image
-                  src="/images/atlan-insights.png"
-                  alt="Cover image for playground section"
-                  width={1600}
-                  height={900}
-                  layout="responsive"
-                />
-              </div>
-              <div className="mt-4">
-                <h3 className="mb-2 text-lg font-semibold leading-snug text-black group-hover:underline lg:text-xl dark:text-white">
-                  Atlan Insights
-                </h3>
-                <p className="mb-2 text-base font-normal leading-snug text-black cursor-pointer lg:text-lg dark:text-white">
-                  How we build insights, our in-house SQL editor to power modern
-                  data teams.
-                </p>
-              </div>
-            </div>
-          </a>
-        </div>
-        <div className="mt-24 lg:mt-36">
-          <div className="home-page-title">Selected Writings</div>
+        <div className="mt-16 lg:mt-36">
+          <div className="home-page-title">Writings</div>
           <WritingList posts={allPosts} />
-          <Link href="/writings" passHref={true}>
-            <button className="mt-4 text-black underline cursor-pointer dark:text-white">
-              Read More
-            </button>
-          </Link>
         </div>
         {/* <div className="mt-12 lg:mt-24">
           <div className="home-page-title">Playground</div>
@@ -247,7 +331,7 @@ export default function Index({ allPosts, allWork }) {
 }
 
 export async function getStaticProps({ preview }) {
-  const allPosts = await getAllPostsForHome(preview)
+  const allPosts = await getAllPosts(preview)
   const allWork = await getAllProjectsForHome(preview)
   return {
     props: { allPosts, allWork },
