43b14ef322e303dd4a644475ece7ab5db675acfc
Shreyas <gshreyas@icloud.com>
2023-09-15 03:08:02 +0530

    writing preview changes

--- components/writing-preview.js
+++ components/writing-preview.js
@@ -5,12 +5,12 @@ export default function writingPreview({ title, udate, excerpt, slug }) {
       <Link as={`/writings/${slug}`} href="/writings/[slug]">
         <div>
           <div className="hidden gap-4 my-4 cursor-pointer md:flex-row md:flex group">
-            <p className="w-24 text-right text-grey-secondary">{udate}</p>
             <div>
-              <h4 className="mb-2 font-medium leading-snug text-white underline transition duration-500 transform group-hover:opacity-70">
+              <div className="mb-2 text-lg font-medium leading-snug text-white underline transition duration-500 transform group-hover:opacity-70">
                 {title}
-              </h4>
-              <p className="mb-2 leading-snug">{excerpt}</p>
+              </div>
+              <p className="mb-2 text-base leading-snug">{excerpt}</p>
+              <p className="text-base text-grey-secondary">{udate}</p>
             </div>
           </div>
 
