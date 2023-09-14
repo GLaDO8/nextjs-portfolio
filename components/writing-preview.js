8214ac6ecf1af03bc161d806394110a2e480cff9
Shreyas <gshreyas@icloud.com>
2023-09-15 03:32:34 +0530

    writing preview changes

--- components/writing-preview.js
+++ components/writing-preview.js
@@ -3,25 +3,13 @@ export default function writingPreview({ title, udate, excerpt, slug }) {
   return (
     <>
       <Link as={`/writings/${slug}`} href="/writings/[slug]">
-        <div>
-          <div className="hidden gap-4 my-4 cursor-pointer md:flex-row md:flex group">
-            <div>
-              <div className="mb-2 text-lg font-medium leading-snug text-white underline transition duration-500 transform group-hover:opacity-70">
-                {title}
-              </div>
-              <p className="mb-2 text-base leading-snug">{excerpt}</p>
-              <p className="text-base text-grey-secondary">{udate}</p>
-            </div>
-          </div>
-
-          <div className="gap-4 my-4 cursor-pointer md:hidden group">
-            <div>
-              <h4 className="mb-2 font-medium leading-snug text-white underline transition duration-500 transform group-hover:opacity-70">
-                {title}
-              </h4>
-              <p className="mb-2 leading-snug">{excerpt}</p>
-              <p className=" text-grey-secondary">{udate}</p>
-            </div>
+        <div className="gap-4 my-4 cursor-pointer">
+          <div>
+            <h4 className="mb-2 font-medium leading-snug text-white underline transition duration-500 transform md:text-lg group-hover:opacity-70">
+              {title}
+            </h4>
+            <p className="mb-2 leading-snug md:text-base">{excerpt}</p>
+            <p className=" text-grey-secondary md:text-base">{udate}</p>
           </div>
         </div>
       </Link>
