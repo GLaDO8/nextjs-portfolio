2d5156c7e2e88315b4880ded43996a32acbcdc2b
Shreyas <gshreyas@icloud.com>
2024-04-16 23:41:57 +0530

    added speed insights package

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '@/styles/index.css'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </>
   )
 }
